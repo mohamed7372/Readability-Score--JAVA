ed910c23c9a00081296eaa969964970ef630bab1
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-08-07 17:51:59 +0200

    update2
    more specific about text if it is hard or easy using charcters special and regexps and shorthands and quantifiers

--- src/readabilityScore/Main.java
+++ src/readabilityScore/Main.java
@@ -8,10 +8,24 @@ public class Main {
 	
 	public static void main(String[] args) {
 		String input = sc.nextLine();
-		if (input.length() > 100) 
-			System.out.println("HARD");
-		else
+		String[] words = input.split("\\s");
+		int nbr = 0, som = 0, nbrPh = 0;
+		
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].matches(".+[.!?]$") || i == words.length - 1){
+				nbr++;
+				som += nbr;
+				nbrPh++;
+				nbr = 0;
+			}
+			else 
+				nbr++;
+		}
+		float moy = (float)som / (float)nbrPh;
+		
+		if (moy <= 10) 
 			System.out.println("EASY");
+		else
+			System.out.println("HARD");
 	}
-
 }
