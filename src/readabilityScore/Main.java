9bc1391a8e126f06a660a8560895aa98caea9ab0
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-08-08 18:27:47 +0200

    update4
    claculate exactly which age category this text using formule : Automated Readability Index,
    Flesch–Kincaid readability tests,
    Simple Measure of Gobbledygook,
    Coleman–Liau index

--- src/readabilityScore/Main.java
+++ src/readabilityScore/Main.java
@@ -9,25 +9,76 @@ public class Main {
 	static final Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		String text = readFile("E:\\GitHub\\Readability-Score--JAVA\\src\\readabilityScore\\" + args[0]);
+		String text = readFile("C:\\Users\\HP\\eclipse-work\\site jet brains\\src\\readabilityScore\\" + args[0]);
 		
 		System.out.println("The text is:");
 		System.out.println(text);
 		
 		String[] words = allWords(text);
 		int nbrWords = words.length;
-		System.out.print("Words: " + nbrWords);
+		System.out.print("\nWords: " + nbrWords);
 		
 		int nbrSen = nbrSenctences(words);
 		System.out.print("\nSentences: " + nbrSen);
 		
 		int nbrCha = nbrCharacters(text);
 		System.out.print("\nCharacters: " + nbrCha);
-
-		double score = 4.71 * ((double)nbrCha / nbrWords) + 0.5 * ((double)nbrWords / nbrSen)- 21.43;
-		System.out.printf("\nThe score is: %.2f",score);
 		
-		System.out.printf("\nThis text should be understood by %s year olds.", catAge((int)Math.ceil(score)));
+		int nbrSly[] = nbrSylabels(words);
+		System.out.print("\nSyllables: " + nbrSly[0]);
+		
+		System.out.print("\nPolysyllables: " + nbrSly[1]);
+		
+		System.out.print("\nEnter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
+		String ch = sc.next();
+
+		int age = 0, age1 = 0, age2 = 0, age3 = 0;
+				
+		switch (ch) {
+		case "ARI": 
+			double score = 4.71 * ((double)nbrCha / nbrWords) + 0.5 * ((double)nbrWords / nbrSen)- 21.43;
+			age = catAge(score);
+			System.out.printf("\nAutomated Readability Index: %.2f (about %d year olds).", score, age);			
+			break;
+		case "FK": 
+			double score1 = 0.39 * ((double)nbrWords / nbrSen) + 11.8 * ((double)nbrSly[0] / nbrWords)- 15.59;
+			age1 = catAge(score1);
+			System.out.printf("\nFlesch–Kincaid readability tests: %.2f (about %d year olds).", score1, age1);
+			break;
+		case "SMOG": 
+			double score2 = 1.043 * Math.sqrt(nbrSly[1] * (30.0 / nbrSen)) + 3.1291;
+			age2 = catAge(score2);
+			System.out.printf("\nSimple Measure of Gobbledygook: %.2f (about %d year olds).", score2, age2);
+			break;
+		case "CL": 
+			double L = (double)nbrCha / nbrWords * 100.0;
+			double S = (double)nbrSen / nbrWords * 100.0;
+			double score3 = 0.0588 * L - 0.296 * S - 15.8;
+			age3 = catAge(score3);
+			System.out.printf("\nColeman–Liau index: %.2f (about %d year olds).", score3, age3);
+			break;
+		default :
+			score = 4.71 * ((double)nbrCha / nbrWords) + 0.5 * ((double)nbrWords / nbrSen)- 21.43;
+			age = catAge(score);
+			System.out.printf("\nAutomated Readability Index: %.2f (about %d year olds).", score, age);		
+			
+			score1 = 0.39 * ((double)nbrWords / nbrSen) + 11.8 * ((double)nbrSly[0] / nbrWords)- 15.59;
+			age1 = catAge(score1);
+			System.out.printf("\nFlesch–Kincaid readability tests: %.2f (about %d year olds).", score1, age1);
+			
+			score2 = 1.043 * Math.sqrt(nbrSly[1] * (30.0 / nbrSen)) + 3.1291; 
+			age2 = catAge(score2);
+			System.out.printf("\nSimple Measure of Gobbledygook: %.2f (about %d year olds).", score2, age2);
+			
+			L = (double)nbrCha / nbrWords * 100.0;
+			S = (double)nbrSen / nbrWords * 100.0;
+			score3 = 0.0588 * L - 0.296 * S - 15.8;
+			age3 = catAge(score3);
+			System.out.printf("\nColeman–Liau index: %.2f (about %d year olds).", score3, age3);
+		}
+		
+		double ageMoy = (age + age1 + age2 + age3) / 4.0;
+		System.out.printf("\n\nThis text should be understood in average by %.2f year olds.", ageMoy);
 	}
 	
 	static String readFile(String pathName) {
@@ -60,38 +111,73 @@ public class Main {
 		}
 		return nb;
 	}
-	static String catAge(int score) {
-		switch (score) {
+	static int catAge(double score) {
+		int scr = 0;
+		if (score % 100 >= 50)
+			scr = (int)Math.ceil(score);
+		else
+			scr = (int)Math.floor(score);
+		
+		switch (scr) {
 		case 1: 
-			return "5-6";
+			return 6;
 		case 2: 
-			return "6-7";
+			return 7;
 		case 3: 
-			return "7-9";
+			return 9;
 		case 4: 
-			return "9-10";
+			return 10;
 		case 5: 
-			return "10-11";
+			return 11;
 		case 6: 
-			return "11-12";
+			return 12;
 		case 7: 
-			return "12-13";
+			return 13;
 		case 8: 
-			return "13-14";
+			return 14;
 		case 9: 
-			return "14-15";
+			return 15;
 		case 10: 
-			return "15-16";
+			return 16;
 		case 11: 
-			return "16-17";
+			return 17;
 		case 12: 
-			return "17-18";
+			return 18;
 		case 13: 
-			return "18-24";
-		case 14: 
-			return "24+";
+			return 24;
 		default:
-			return null;
+			return 0;
 		}
+	}
+	static int[] nbrSylabels(String[] words) {
+		int nbr = 0;
+		int nbrP = 0;
+		for (int i = 0; i < words.length; i++) {
+			String s = words[i];
+			int c = 0;
+			// count all vowel
+			for (int j = 0; j < s.length(); j++) {
+				if (s.substring(j, j + 1).matches("[aeiouyAEIOUY]"))
+					c++;
+			}
+			// count 1 when find two sylables together
+			for (int j = 0; j < s.length() - 1; j++) {
+				if (s.substring(j, j + 2).matches("[aeiouyAEIOUY][aeiouyAEIOUY]"))
+					c--;
+			}
+			// if the last letter is 'e' does not count
+			if(s.matches(".+e[?!,.]?"))
+				c--;
+			// if zero vowel that is mean one sylabels
+			if (c == 0)
+				c++;
+			// som sylabels
+			nbr += c;
+			
+			// polysylabels
+			if (c >= 3)
+				nbrP++;
+		}
+		return new int[] {nbr, nbrP};
 	}
 }
